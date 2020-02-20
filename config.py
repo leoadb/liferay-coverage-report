fef34a4154b0b3406b0c1b6b9ef7c5df7dfe5c5b
Leonardo Barros <leonardo.barros@liferay.com>
2020-02-20 13:51:36 -0300

    initial version

---
+++ config.py
@@ -0,0 +1,5 @@
+INTEGRATION_TEST_ENABLED=False
+LIFERAY_BUNDLES=['data-engine-rest-impl','data-engine-rest-api','data-engine-service','data-engine-api']
+LIFERAY_HOME='/Users/leonardo/Liferay/liferay-portal'
+LIFERAY_MODULE='modules/apps/data-engine'
+UNIT_TEST_ENABLED=True
---
+++ coverage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+import config
+import shutil
+
+class_names = []
+
+path = os.path.join(config.LIFERAY_HOME, config.LIFERAY_MODULE)
+
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        for bundle in config.LIFERAY_BUNDLES:
+            if os.path.join(bundle, 'src/main/java') in r:
+                name = file.split('.')[0]
+                with open(os.path.join(r, file)) as of:
+                    if 'class {}'.format(name) in of.read():
+                        class_names.append(name)
+                break
+
+print(','.join(map(str, class_names)))
+
+result = -1
+
+shutil.rmtree(os.path.join(config.LIFERAY_HOME, 'jacoco'), ignore_errors=True)
+
+if config.UNIT_TEST_ENABLED:
+    result = os.system(os.path.join(config.LIFERAY_HOME, 'gradlew -p {} test -Djunit.code.coverage=true'.format(path)))
+
+if result == 0:
+    result = os.system('ant -f {} generate-code-coverage-report -Dclass.names={}'.format(config.LIFERAY_HOME, ','.join(map(str, class_names))))
+
+print(result)
